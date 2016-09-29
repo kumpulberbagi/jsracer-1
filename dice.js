9fd16c9f61a3701a84e9bb9736a6f26e037ba557
Aji Lantang Mardika <email": null>
2016-09-29 20:49:22 +0700

    js racer masih pr banyak

--- dice.js
+++ dice.js
@@ -2,11 +2,10 @@
 
 class Dice {
   constructor() {
-
+    this.length = 6
   }
   roll() {
-    
+    return Math.floor(Math.random()*2)+1
   }
 }
-
 export default Dice
--- js_racer.js
+++ js_racer.js
@@ -4,26 +4,76 @@ import Dice from "./dice.js"
 
 class JSRacer {
   constructor(players, length, sides) {
-
+    this.players = players
+    this.length = length
+    this.sides = sides
   }
   print_board() {
+  var board = []
+    for (var i = 0; i < this.players; i++) {
+      board.push([])
+      // boar[i][0] = players[i]
+      for (var k = 0; k <this.length; k++) {
+        board[i].push("| ")
+      }
+    }
+    return board
+  }
 
+  print_line() {
+    var track = this.print_board();
+    var start = [];
+    //console.log(track.length);
+    for (var i = 0; i < this.players; i++) {
+      track[i][0] = "player"+(i+1);
+    }
+    for (var i = 0; i < this.players; i++) {
+        start.push(track[i].join(''));
+    }
+    // for (var i = 0; i < start.length; i++) {
+    //   console.log(start[i]);
+    // }
+    return start
+    //return print_line(player)
   }
-  print_line(player, pos) {
 
-  }
-  advanced_player(player) {
+  advanced_player() {
+    var track = this.print_board();
 
+    var start = [];
+    var count = 0
+    var random = new Dice
+    for (var i = 0; i < this.players; i++) {
+      for (var j = 0; j < this.length; j++) {
+        count+=random.roll()+j
+        console.log(track);
+        if (count >= this.length) {
+          return track[i][count] +"win"
+        }
+        track[i][count] = "player"+(i+1);
+      }
+    //
+    //   return test.advanced_player()
+    }
+    console.log(track)
+  //
+  //
+  //   //return print_line(player)
   }
-  finished() {
-
-  }
-  winner() {
-
-  }
-  reset_board() {
-    console.log("\x1B[2J")
-  }
+  //
+  // finished() {
+  //
+  // }
+  // winner() {
+  //
+  // }
+  // reset_board() {
+  //   console.log("\x1B[2J")
+  // }
 }
 
+var test = new JSRacer(2,16,6)
+// var test2 = new JSRacer(2,16,4)
+console.log(test.advanced_player());
+// console.log(test2.advanced_player());
 export default JSRacer
