5bfd7e486e2e59628dcfd1e2c60e80f9976c8773
Ken Duigraha Putra & Izhharuddin Syanmil <pair+kenduigraha.syanmil@hacktiv8.com>
2016-09-29 13:52:35 +0700

    mass print board done

--- js_racer.js
+++ js_racer.js
@@ -9,57 +9,63 @@ class JSRacer {
     this.sides = sides;
     this.lastPlayer = '';
   }
+
   print_board() {
-    console.log(this.print_line(players, pos));
+    return this.print_line(this.players, [1, 2, 3])
   }
-  print_line(player, pos) {
+
+  print_line(players, pos) {
     var arr = [];
-    for (var i = 0; i <player.length; i++){
+    for (var i = 0; i <players.length; i++){
       arr[i] = [];
-      for (var j = 0; j<this.lengths; i++){
-        var x = arr[i];
-        var y = x[j];
-        y.push("|  |");
+      for (var j = 0; j<this.lengths; j++){
+        arr[i][j] = " | ";
       }
     }
 
-    for(var j = 0 ; j < player ; i++){
-      arr.splice(pos[j], 1, player[j]);
+    for(var g = 0 ; g < players.length ; g++){
+      arr[g].splice(pos[g], 1, players[g]);
     }
-    return arr;
-  }
+    //console.log(arr)
 
-  advanced_player(players) {
-    var pos = [];
-
-    for(var x = 0 ; x < player.length ; x++){
-      pos[x] = 0;
+    for(var z = 0 ; z < arr.length ; z++){
+      console.log(arr[z].toString().replace(/,/g,'') + "\n");
     }
-
-    for (var i = 0; i <player; i++){
-      var dadu = new Dice(this.sides);
-      pos[i] = pos[i] + dadu;
-    }
-    return pos;
+ 
   }
-
-  finished() {
-    if( arr.length-1 != "| |"){
-      advanced_player(players);
-    }else{
-      return this.lastPlayer;
-    }
-  }
-  winner() {
-    if(finished() == true){
-      return "The Winner is " + this.lastPlayer;
-    }
-  }
-  reset_board() {
-    console.log("\x1B[2J")
-  }
+  //
+  // advanced_player(players) {
+  //   var pos = [];
+  //
+  //   for(var x = 0 ; x < player.length ; x++){
+  //     pos[x] = 0;
+  //   }
+  //
+  //   for (var i = 0; i <player; i++){
+  //     var dadu = new Dice(this.sides);
+  //     pos[i] = pos[i] + dadu;
+  //   }
+  //   return pos;
+  // }
+  //
+  // finished() {
+  //   if( arr.length-1 != "| |"){
+  //     advanced_player(players);
+  //   }else{
+  //     return this.lastPlayer;
+  //   }
+  // }
+  // winner() {
+  //   if(finished() == true){
+  //     return "The Winner is " + this.lastPlayer;
+  //   }
+  // }
+  // reset_board() {
+  //   console.log("\x1B[2J")
+  // }
 }
 
 export default JSRacer
 var test = new JSRacer(['a', 'b', 'c'],20,6);
-console.log(test.print_line(['a', 'b', 'c'], [5, 3, 2]));
+// console.log(
+console.log(test.print_board())
