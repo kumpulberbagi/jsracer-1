9dc235092535fc457e1e1ddabd6b9667a89d2cb0
Toni <tonichen184@gmail.com>
2016-10-02 12:51:59 +0700

    Update Js-Racer.js
    Revisi JS-Racer

--- js_racer.js
+++ js_racer.js
@@ -1,160 +1,97 @@
 "use strict"
+class Dice {
+  constructor() {
+
+  }
+  roll() {
+      return Math.floor(Math.random()*2)+1
+  }
+}
+var dice = new Dice()
 
 class JSRacer {
-  constructor(PlayerA, PlayerB, PlayerC, PlayerALine, PlayerBLine, PlayerCLine) {
-    this.tampungBoard = []
-    this.PlayerA = PlayerA
-    this.PlayerB = PlayerB
-    this.PlayerC = PlayerC
-    this.PlayerALine = PlayerALine
-    this.PlayerBLine = PlayerBLine
-    this.PlayerCLine = PlayerCLine
-    this.PlayerCurPosA = 0
-    this.PlayerCurPosB = 0
-    this.PlayerCurPosC = 0
-    this.onPlaying = true
+  constructor(players,length,position) {
+    this.position = position
+    this.players = players
+    this.length = length
+    this.sides = []
+    this.isPlaying = true
   }
-  runner() {
-    this.print_board_and_Get_Movement_and_getWinner()
-  }
-  print_board_and_Get_Movement_and_getWinner() {
-    for (var i = 0; i < 3; i++) {
-      this.tampungBoard.push([' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '])
-    }
-    // Place player
-    this.PlayerA = this.tampungBoard[0][this.PlayerCurPosA] = 'A'
-    this.PlayerB = this.tampungBoard[1][this.PlayerCurPosB] = 'B'
-    this.PlayerC = this.tampungBoard[2][this.PlayerCurPosC] = 'C'
-      // player line
-    this.PlayerLineA = this.tampungBoard[0]
-    this.PlayerLineB = this.tampungBoard[1]
-    this.PlayerLineC = this.tampungBoard[2]
-      //Random movement player A
-    for (var i = 0; i < this.PlayerLineA.length; i++) {
-      if (this.tampungBoard[0][this.PlayerCurPosA] === 'A' ||
-        this.tampungBoard[0][this.PlayerCurPosA + randomMovement()] === 'A') {
-        this.PlayerA = this.tampungBoard[0][this.PlayerCurPosA] = ''
-        this.PlayerCurPosA += randomMovement()
-        this.PlayerA = this.tampungBoard[0][this.PlayerCurPosA] = 'A'
-        if (i == 4) {
-          // if (this.tampungBoard[0][this.PlayerCurPosA] === 'A') {
-          //   console.log("Position A1 : " + this.PlayerCurPosA);
-          //   console.log("A Winner");
-          // }
-
-          if (this.PlayerCurPosA >= 8) {
-            this.onPlaying = false
-            console.log("Position A : " + this.PlayerCurPosA);
-            console.log("A Winner");
-          }
-
-          break
-        }
-        console.log(this.tampungBoard);
-      } else {
-        this.PlayerCurPosA += randomMovement()
-        this.PlayerA = this.tampungBoard[0][this.PlayerCurPosA] = 'A'
-        if (i == 4) {
-          if (this.PlayerCurPosA >= 8) {
-            this.onPlaying = false
-            console.log("Position A : " + this.PlayerCurPosA);
-            console.log("A Winner");
-          }
-          // if (this.tampungBoard[0][this.PlayerCurPosA] === 'A') {
-          //   console.log("Position A2 : " + this.PlayerCurPosA);
-          //   console.log("A Winner");
-          // }
-          break
-        }
-        console.log(this.tampungBoard);
-      }
-      //Random movement player B
-      if (this.tampungBoard[1][this.PlayerCurPosB] === 'B' || this.tampungBoard[1][this.PlayerCurPosB + randomMovement()] === 'B') {
-        this.PlayerB = this.tampungBoard[1][this.PlayerCurPosB] = ''
-        this.PlayerCurPosB += randomMovement()
-        this.PlayerB = this.tampungBoard[1][this.PlayerCurPosB] = 'B'
-        if (i == 4) {
-          // if (this.tampungBoard[0][this.PlayerCurPosB] === 'B') {
-          //   console.log("Position B : " + this.PlayerCurPosB);
-          //   console.log("B Winner");
-          // }
-          if (this.PlayerCurPosB >= 8) {
-            this.onPlaying = false
-            console.log("Position B : " + this.PlayerCurPosB);
-            console.log("B Winner");
-          }
-          break
-        }
-        console.log(this.tampungBoard);
-      } else {
-        this.PlayerCurPosB += randomMovement()
-        this.PlayerB = this.tampungBoard[1][this.PlayerCurPosB] = 'B'
-        if (i == 4) {
-          // if (this.tampungBoard[0][this.PlayerCurPosB] === 'B') {
-          //   console.log("Position B : " + this.PlayerCurPosB);
-          //   console.log("B Winner");
-          // }
-          if (this.PlayerCurPosB >= 8) {
-            this.onPlaying = false
-            console.log("Position B : " + this.PlayerCurPosB);
-            console.log("B Winner");
-          }
-          break
-        }
-        console.log(this.tampungBoard);
-      }
-      //Random movement player C
-      if (this.tampungBoard[2][this.PlayerCurPosC] === 'C' || this.tampungBoard[2][this.PlayerCurPosC + randomMovement()] === 'C') {
-        this.PlayerC = this.tampungBoard[2][this.PlayerCurPosC] = ''
-        this.PlayerCurPosC += randomMovement()
-        this.PlayerC = this.tampungBoard[2][this.PlayerCurPosC] = 'C'
-        if (i == 4) {
-          // if (this.tampungBoard[0][this.PlayerCurPosC] === 'C') {
-          //   console.log("Position C : " + this.PlayerCurPosC);
-          //   console.log("C Winner");
-          // }
-          if (this.PlayerCurPosC >= 8) {
-            this.onPlaying = false
-            console.log("Position C : " + this.PlayerCurPosC);
-            console.log("C Winner");
-          }
-          //
-          // if(this.PlayerCurPosC == 8) {
-          //
-          // }
-          //
-          break
-        }
-        console.log(this.tampungBoard);
-      } else {
-        this.PlayerCurPosC += randomMovement()
-        this.PlayerC = this.tampungBoard[2][this.PlayerCurPosC] = 'C'
-        if (i == 4) {
-          // if (this.tampungBoard[0][this.PlayerCurPosC] === 'C') {
-          //   console.log("Position C : " + this.PlayerCurPosC);
-          //   console.log("C Winner");
-          // }
-          if (this.PlayerCurPosC >= 8) {
-            this.onPlaying = false
-            console.log("Position C : " + this.PlayerCurPosC);
-            console.log("C Winner");
-          }
-          break
-        }
-        console.log(this.tampungBoard);
+  print_board() {
+    for(var i=0;i<this.length;i++)
+    {
+      this.sides[i] = []
+      for(var j=0;j<10;j++)
+      {
+        this.sides[i].push(' ')
       }
     }
-
+    return this.sides
   }
 
+  print_line() {
+    for(var i=0;i<this.players.length;i++)
+    {
+      this.sides[i][this.position] = this.players[i]
+    }
+    return this.sides
+  }
+
+  advanced_player() {
+    this.count = 0
+    this.countLine = 0
+  while(this.isPlaying){
+          this.count++
+    for(var i=0;i<this.players.length;i++)
+      {
+        for(var j=0;j<=10;j++)
+        {
+          this.countLine++
+          if(this.sides[i][j] === this.players[i])
+          {
+             this.sides[i][j]=''
+           }
+         }
+         this.sides[i][dice.roll()+this.count] = this.players[i]
+      }
+      this.sleep(500)
+      this.reset_board()
+
+      console.log('-----------------------------------------------------------------');
+      console.log(this.sides)
+
+
+      if(this.count >= 8){
+    this.isPlaying = false
+        for(var i=0;i<this.players.length;i++){
+          if(this.sides[i].length > 10){
+              console.log('Winner '+this.players[i]);
+            }
+
+      }
+
+      }
+}
+
+}
   reset_board() {
     console.log("\x1B[2J")
   }
+  sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds) {
+        break;
+      }
+    }
+  }
 
+  runner()
+  {
+    this.advanced_player()
+  }
 }
-var jsRacer = new JSRacer()
-jsRacer.runner()
 
-function randomMovement() {
-  return Math.floor(Math.random() * 2) + 1
-}
+var jsracer = new JSRacer(['A','B','C'],3,0)
+jsracer.print_board()
+jsracer.runner()
